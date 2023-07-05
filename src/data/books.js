09f4dc90b01bb5f7b98795cc734a411ecbb9ae53
Lenin A. Nava <35464086+leninnava@users.noreply.github.com>
2023-07-04 23:20:26 -0400

    boop, initial commit

---
+++ src/data/books.js
@@ -0,0 +1,44 @@
+export const books = [
+	{
+		id: 1,
+		author: 'dummy author',
+		title: 'The Pragmatic Programmer',
+		notes: 'dummy notes',
+		img: 'https://images-na.ssl-images-amazon.com/images/I/41as+WafrFL._SX258_BO1,204,203,200_.jpg'
+	},
+	{
+		id: 2,
+		author: 'dummy author',
+		title: 'Eloquent JavaScript',
+		notes: 'dummy notes',
+		img: 'https://eloquentjs-es.thedojo.mx/img/cover.jpg'
+	},
+	{
+		id: 3,
+		author: 'dummy author',
+		title: 'Inherit The Stars',
+		notes: 'dummy notes',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1339677216i/776489.jpg'
+	},
+	{
+		id: 4,
+		author: 'dummy author',
+		title: 'Animal Farm',
+		notes: 'dummy notes',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1424037542i/7613.jpg'
+	},
+	{
+		id: 5,
+		author: 'dummy author',
+		title: 'The Metamorphosis',
+		notes: 'dummy notes',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1646444605i/485894.jpg'
+	},
+	{
+		id: 6,
+		author: 'dummy author',
+		title: 'The Little Prince',
+		notes: 'dummy notes',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1367545443i/157993.jpg'
+	}
+]
---
+++ src/utils/startNavigationTransition.js
@@ -0,0 +1,33 @@
+const checkAnimationAPISupport = () => {
+	if (document.startViewTransition) {
+		return true
+	}
+	console.log('Web Transitions API not supported.')
+	return false
+}
+
+const fetchPage = async (url) => {
+	const response = await fetch(url)
+	const text = await response.text()
+
+	const [, data] = text.match(/<body[^>]*>([\s\S]*)<\/body>/i)
+	return data
+}
+
+export const startNavigationTransition = () => {
+	if (checkAnimationAPISupport()) {
+		window.navigation.addEventListener('navigate', (event) => {
+			const destinationURL = new URL(event.destination.url)
+			if (location.origin !== destinationURL.origin) return
+			event.intercept({
+				async handler () {
+					const data = await fetchPage(destinationURL.pathname)
+					document.startViewTransition(() => {
+						document.body.innerHTML = data
+						document.documentElement.scrollTop = 0
+					})
+				}
+			})
+		})
+	}
+}
